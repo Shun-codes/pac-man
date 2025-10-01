a648e9d6d7caf015e81b6fb7104bf974b22b5314
Shun Lembrez <shun_lembrez@ens.univ-artois.fr>
2025-10-01 14:51:08 +0200

    Ajout d'une enum de couleur pour différencier les fantômes entre eux

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/Ghost.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/Ghost.java
@@ -7,6 +7,9 @@
 
 package fr.univartois.butinfo.r304.pacman.model.animated;
 
+import fr.univartois.butinfo.r304.pacman.model.IAnimated;
+import fr.univartois.butinfo.r304.pacman.model.PacmanGame;
+import fr.univartois.butinfo.r304.pacman.view.Sprite;
 
 /**
  * Le type Ghost
@@ -15,7 +18,54 @@ package fr.univartois.butinfo.r304.pacman.model.animated;
  *
  * @version 0.1.0
  */
-public class Ghost {
+public class Ghost extends AbstractAnimated{
+    
+    /**
+     * L'attribut color pour les fantômes
+     */
+    GhostColor color;
+    
+    /**
+     * Crée une nouvelle instance de Ghost.
+     * @param game de type {@link PacmanGame}
+     * @param xPosition de type {@link Double}
+     * @param yPosition de type {@link Double}
+     * @param sprite de type {@link Sprite}
+     */
+    protected Ghost(PacmanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 
+
+    /**
+     * Donne l'attribut color de cette instance de Ghost.
+     *
+     * @return L'attribut color de cette instance de Ghost.
+     */
+    public GhostColor getColor() {
+        return color;
+    }
+
+    
+    /**
+     * Modifie l'attribut color de cette instance de Ghost.
+     *
+     * @param color La nouvelle valeur de l'attribut color pour cette instance de Ghost.
+     */
+    public void setColor(GhostColor color) {
+        this.color = color;
+    }
+
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.r304.pacman.model.IAnimated#onCollisionWith(fr.univartois.butinfo.r304.pacman.model.IAnimated)
+     */
+    @Override
+    public void onCollisionWith(IAnimated other) {
+        // TODO Auto-generated method stub.   
+    }
+    
 }
 
