31af3e69c56f3573e5d4c7c4aaa75b86a09f3fc3
Romain THIBAUT <romain_thibaut1@ens.univ-artois.fr>
2025-10-01 15:49:17 +0200

    Ajout de la partie 4, non finis, actuellement à l'ajout des murs

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/map/CardGenerator.java
@@ -0,0 +1,21 @@
+/**
+ * Ce fichier fait partie du projet projet-2025-2026.
+ *
+ * (c) 2025 romain.thibaut
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.pacman.model.map;
+
+
+/**
+ * Le type CardGenerator
+ *
+ * @author romain.thibaut
+ *
+ * @version 0.1.0
+ */
+public class CardGenerator {
+
+}
+
