3a2d07bdf139fe3a4e4ea1424ac04f3a3a694e45
Shun Lembrez <shun_lembrez@ens.univ-artois.fr>
2025-10-01 14:23:19 +0200

    Creation de la classe Ghost non complété

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/Ghost.java
@@ -0,0 +1,21 @@
+/**
+ * Ce fichier fait partie du projet projet-2025-2026.
+ *
+ * (c) 2025 shun.lembrez
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.pacman.model.animated;
+
+
+/**
+ * Le type Ghost
+ *
+ * @author shun.lembrez
+ *
+ * @version 0.1.0
+ */
+public class Ghost {
+
+}
+
