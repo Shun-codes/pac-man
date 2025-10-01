22b0f50230db74949e3662e78373a9a8c7579cae
Gros Timothee <timothee_gros@ens.univ-artois.fr>
2025-10-01 14:22:16 +0200

    création de la classe PacMan

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/PacMan.java
@@ -0,0 +1,23 @@
+/**
+ * Ce fichier fait partie du projet projet-2025-2026.
+ *
+ * (c) 2025 timothee.gros
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.r304.pacman.model.animated;
+
+import fr.univartois.butinfo.r304.pacman.model.PacmanGame;
+import fr.univartois.butinfo.r304.pacman.view.Sprite;
+
+/**
+ * Le type PacMan
+ *
+ * @author timothee.gros
+ *
+ * @version 0.1.0
+ */
+public class PacMan extends AbstractAnimated{
+
+}
+
