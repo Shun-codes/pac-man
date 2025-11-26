04d276a2852819f8138c8cb8ffaa51a5e73105f6
Romain THIBAUT <romain_thibaut1@ens.univ-artois.fr>
2025-11-26 15:13:58 +0100

    Suppression de ligne inutile suite à abstractFactory, createMap. #31

--- src/main/java/fr/univartois/butinfo/r304/pacman/PacmanApplication.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/PacmanApplication.java
@@ -69,7 +69,6 @@ public final class PacmanApplication extends Application {
         // On crée ensuite le jeu, que l'on lie au contrôleur.
         PacmanGame game = new PacmanGame(
                 GAME_WIDTH, GAME_HEIGHT, SpriteStore.getInstance(), NB_GHOSTS);
-        game.setGenerator(new CardGeneratorFixed(CardGeneratorEmpty.getInstance()));
         controller.setGame(game);
         game.setController(controller);
         game.prepare();
